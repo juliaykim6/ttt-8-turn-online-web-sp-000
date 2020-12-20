9ed14dffedfaa50a7794b37b385fe37af7217a3a
Julia Kim <juliaykim6@gmail.com>
2020-12-20 21:44:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,7 +37,7 @@ def position_taken? (board, index)
     user_input = gets.strip
     index = input_to_index(user_input)
     if valid_move?(board, index)
-      move(board, index, value = "X")
+        move(board, index, value = "X")
         display_board(board)
     else turn(board)
     end
