cc9b7f38fac9004db0edb7bbd713a4a376f45e81
Julia Kim <juliaykim6@gmail.com>
2020-12-20 21:23:05 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,3 +15,21 @@ end
 def move(board, index, character = "X")
   board[index] = character
 end
+
+def valid_move? (board, index)
+  if (index).between?(0,8) && !position_taken?(board, index)
+    return true
+  else
+    return false
+  end
+end
+
+def position_taken? (board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else board[index] == "X" || board[index] == "O"
+    return true
+  end
+  end
+
+  
